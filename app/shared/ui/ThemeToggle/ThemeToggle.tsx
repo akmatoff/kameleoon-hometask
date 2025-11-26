45d1dc1467b732d89b56dcaa0a4a57bb291ffc64
akmatoff <azim.akmatov1@gmail.com>
2025-11-26 21:16:55 +0600

    click animation on theme toggle

--- app/shared/ui/ThemeToggle/ThemeToggle.tsx
+++ app/shared/ui/ThemeToggle/ThemeToggle.tsx
@@ -2,14 +2,19 @@ import { FiSun, FiMoon } from "react-icons/fi";
 import useTheme from "~/shared/hooks/useTheme";
 
 import styles from "./ThemeToggle.module.scss";
+import { motion } from "motion/react";
 
 export const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme} className={styles.button}>
+    <motion.button
+      whileTap={{ scale: 0.95 }}
+      onClick={toggleTheme}
+      className={styles.button}
+    >
       {theme === "dark" ? <FiSun /> : <FiMoon />}
-    </button>
+    </motion.button>
   );
 };
 
